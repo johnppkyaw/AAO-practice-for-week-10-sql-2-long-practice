e352b427f106b44ed7d81dc974da6ce161ffe470
John <jjpyae@gmail.com>
2023-09-20 20:52:27 -0700

    completes phase 3

--- sql-practice/phase-3/query-1.sql
+++ sql-practice/phase-3/query-1.sql
@@ -1,2 +1,6 @@
 -- Find Hermione's cats
--- Your code here
+-- Find Hermione's cats
+SELECT cats.name FROM cats
+  JOIN cat_owners ON (cat_id = cats.id)
+  JOIN owners ON (owners.id = owner_id)
+  WHERE first_name = 'Hermione';
--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,13 @@
 -- Find All the Toys for Hermione's cats
--- Your code here
+-- 3 JOINS
+SELECT toys.name FROM toys
+  JOIN cats ON (toys.cat_id = cats.id)
+  JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+  JOIN owners ON (owner_id = owners.id)
+  WHERE first_name = 'Hermione';
+
+-- 2 JOINS
+SELECT toys.name FROM toys
+  JOIN cat_owners ON (cat_owners.cat_id = toys.cat_id)
+  JOIN owners ON (owner_id = owners.id)
+  WHERE first_name = 'Hermione';
